c6e90a66007a5d95a208c733acc7104e38b660c7
Ron Smith <ron.smith@thataintworking.com>
2015-10-11 22:53:07 -0500

    it works on the bench, moving to the dalek

--- python/i2c_cmds.py
+++ python/i2c_cmds.py
@@ -10,21 +10,29 @@ from smbus import SMBus
 
 MOTOR_CONTROLLER_ADDRESS = 0x4d	 # aka 77 or 'M'
 
-WHICH = 'LRB'  # Left, Right or Both
-DIR = 'FR'  # Forward or Backward
+LEFT = 'L'
+RIGHT = 'R'
+BOTH = 'B'
+FORWARD = 'F'
+REVERSE = 'R'
 
+WHICH = LEFT + RIGHT + BOTH
+DIR = FORWARD + REVERSE
+
+DIRECTION = 'D'
+SPEED = 'S'
 
 bus = SMBus(1)
 
 
 def direction(w, d):
     if w[0] in WHICH and d[0] in DIR:
-        bus.write_i2c_block_data(MOTOR_CONTROLLER_ADDRESS, ord('D'), [ord(w[0]), ord(d[0])])
+        bus.write_i2c_block_data(MOTOR_CONTROLLER_ADDRESS, ord(DIRECTION), [ord(w[0]), ord(d[0])])
 
 
 def speed(w, n):
     if w[0] in WHICH and n >= 0 and n <= 10:
-        bus.write_i2c_block_data(MOTOR_CONTROLLER_ADDRESS, ord('S'), [ord(w[0]), 48+n])
+        bus.write_i2c_block_data(MOTOR_CONTROLLER_ADDRESS, ord(SPEED), [ord(w[0]), 48+n])
     else:
         raise Exception("Invalid values")
 
