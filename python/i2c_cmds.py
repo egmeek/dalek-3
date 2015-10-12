c956eb9e8f23458ce643bd94dbf2d0bfa59d9913
Ron Smith <ron.smith@thataintworking.com>
2015-10-11 21:54:03 -0500

    updated i2c_cmds.py, add .gitignore

--- python/i2c_cmds.py
+++ python/i2c_cmds.py
@@ -1,5 +1,6 @@
-# i2c_cmds.py
 # coding: UTF-8
+
+# i2c_cmds.py
 # A file for importing some basic I2C commands for the dalek
 # Author: Ron Smith
 # Copyright ©2015 That Ain't Working, All Rights Reserved
@@ -7,19 +8,24 @@
 from smbus import SMBus
 
 
-MOTOR_CONTROLLER_ADDRESS = 77
+MOTOR_CONTROLLER_ADDRESS = 0x4d	 # aka 77 or 'M'
+
+WHICH = 'LRB'  # Left, Right or Both
+DIR = 'FB'  # Forward or Backward
 
 
 bus = SMBus(1)
 
 
-def direction(d):
-    bus.write_i2c_block_data(MOTOR_CONTROLLER_ADDRESS, ord('{'),
-                             [ord(b) for b in 'DB%s}' % d])
+def direction(w, d):
+    if w[0] in WHICH and d[0] in DIR:
+        bus.write_i2c_block_data(MOTOR_CONTROLLER_ADDRESS, ord('D'), [ord(w[0]), ord(d[0])])
 
 
-def speed(n):
-    bus.write_i2c_block_data(MOTOR_CONTROLLER_ADDRESS, ord('{'),
-                             [ord(b) for b in 'SB%d}' % n])
+def speed(w, n):
+    if w[0] in WHICH and n >= 0 and n <= 10:
+        bus.write_i2c_block_data(MOTOR_CONTROLLER_ADDRESS, ord('S'), [ord(w[0]), 48+n])
+    else:
+        raise Exception("Invalid values")
 
 
